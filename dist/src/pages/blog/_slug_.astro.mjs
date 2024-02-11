70f3830529541cb4afcb9ee6ec50ac2f80ff6688
kisjam <kisjam@gmail.com>
2024-02-11 10:23:22 -0500

    公開

--- dist/src/pages/blog/_slug_.astro.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../../../renderers.mjs';
-export { onRequest } from '../../../_empty-middleware.mjs';
-import '../../../chunks/astro.bbd08310.mjs';
-import 'html-escaper';
-
-const page = () => import('../../../chunks/pages/_slug_.astro.07748c0e.mjs');
-
-export { page };
--- dist/src/pages/index.astro.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-export { onRequest } from '../../_empty-middleware.mjs';
-import '../../chunks/astro.bbd08310.mjs';
-import 'html-escaper';
-
-const page = () => import('../../chunks/pages/index.astro.bca26699.mjs');
-
-export { page };
